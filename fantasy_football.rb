3f8c2b056f5f7a080e6e6f0e731fea8d0b4713b7
Andrew Sheehan <sheehan.andrew@gmail.com>
2014-11-05 14:19:16 -0500

    added te and k support; updated to week 10

--- fantasy_football.rb
+++ fantasy_football.rb
@@ -20,16 +20,20 @@ require 'open-uri'
 
 # get random seed via website with text/numbers
 espn_uris = {
-    'rb'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek9RB/fantasy-football-Week-9-fantasy-football-running-back-rankings',
-    'wr'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek9WR/fantasy-football-Week-9-fantasy-football-wide-receiver-rankings',
-    'qb'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek9QB/fantasy-football-Week-9-fantasy-football-quarterback-rankings',
-    'def' => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek9DST/fantasy-football-Week-9-fantasy-football-defense-rankings',
+    'rb'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10RB/fantasy-football-Week-10-fantasy-football-running-back-rankings',
+    'wr'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10WR/fantasy-football-Week-10-fantasy-football-wide-receiver-rankings',
+    'qb'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10QB/fantasy-football-Week-10-fantasy-football-quarterback-rankings',
+    'def' => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10DST/fantasy-football-Week-10-fantasy-football-defense-rankings',
+    'te'  => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10TE/fantasy-football-week-10-fantasy-football-tight-end-rankings',
+    'k'   => 'http://espn.go.com/fantasy/football/story/_/page/14ranksWeek10K/fantasy-football-week-10-fantasy-football-kicker-rankings',
 }
 mengs_uris = {
     'rb'  => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=8&CATEGORY=freeagent&POSITION=RB&DISPLAY=points&TEAM=*',
     'wr'  => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=8&CATEGORY=freeagent&POSITION=WR&DISPLAY=points&TEAM=*',
     'qb'  => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=8&CATEGORY=freeagent&POSITION=QB&DISPLAY=points&TEAM=*',
     'def' => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=8&CATEGORY=freeagent&POSITION=Def&DISPLAY=points&TEAM=*',
+    'te'  => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=9&CATEGORY=freeagent&POSITION=TE&DISPLAY=points&TEAM=*',
+    'k'   => 'http://football9.myfantasyleague.com/2014/top?L=16839&SEARCHTYPE=BASIC&COUNT=32&YEAR=2014&START_WEEK=1&END_WEEK=9&CATEGORY=freeagent&POSITION=PK&DISPLAY=points&TEAM=*',
 }
 
 type = ARGV[0] ? ARGV[0] : ''
